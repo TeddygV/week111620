00e39aa3c2597767f047714010ae94ef40fa87d5
Tibi <tvoinea@my.bcit.com>
2022-11-15 15:16:56 -0800

    working with math.pi in javascript

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
 // const greet = fuction (user) => 
-function greet(user, time = 'day') 
-{
-    console.log(`Hello, ${time} ${user}!`);
-}
+// function greet(user, time = 'day') 
+// {
+//     console.log(`Hello, ${time} ${user}!`);
+// }
 
-greet("Neda");
-greet("Neda", "morning", "evening");
+// greet("Neda");
+// greet("Neda", "morning", "evening");
+let r = prompt("enter a radius");
+console.log(r)
+function circleArea(radius) {
+    return Math.PI * radius * radius;
+}
+let area = circleArea(r).toFixed(2);
+alert(`The area of a circle with radius ${r} is ${area}`);
