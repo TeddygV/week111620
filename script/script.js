4e04f47152dcceffda99f6bc64039d4073e272ed
Tibi <tvoinea@my.bcit.com>
2022-11-15 15:33:33 -0800

    update paragraphs based on area

--- script/script.js
+++ script/script.js
@@ -6,10 +6,24 @@
 
 // greet("Neda");
 // greet("Neda", "morning", "evening");
+
+
+
 let r = prompt("enter a radius");
 console.log(r)
 function circleArea(radius) {
+    const radiusP = document.querySelector("#radius");
+    const resultP = document.querySelector("#result");
+    let area = circleArea(r).toFixed(2);
+    radiusP.textContent = radius;
+    let calculatedarea = Math.PI * radius * radius;
+    resultP.textContent = calculatedarea;
+    return calculatedarea;
+
+    
     return Math.PI * radius * radius;
+    
 }
 let area = circleArea(r).toFixed(2);
 alert(`The area of a circle with radius ${r} is ${area}`);
+
